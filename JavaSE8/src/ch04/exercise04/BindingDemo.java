69464d1c213f7218a959bfba2341db37463c7544
zDpxq6 <tsuguka@gmail.com>
2015-08-06 23:21:54 +0900

    added

--- JavaSE8/src/ch04/exercise04/BindingDemo.java
+++ JavaSE8/src/ch04/exercise04/BindingDemo.java
@@ -1,25 +1,33 @@
 package ch04.exercise04;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /*4.
 91ページの4.5節「バインディング」のプログラムについて,
 円が真ん中に配置され, シーンの4辺の少なくとも2つの辺に常に接するように機能拡張しなさい.
 */
+
 public class BindingDemo extends Application {
+	private static final double X = 100;
+	private static final double Y = 100;
+	private static final double RADIUS = 100;
+	private static final Color CIRCLE_COLOR = Color.RED;
 	@Override
 	public void start(Stage stage) {
-		TextArea shipping = new TextArea();
-		TextArea billing = new TextArea();
-		billing.textProperty().bindBidirectional(shipping.textProperty());
-		VBox root = new VBox();
-		root.getChildren().addAll(new Label("Shipping"), shipping, new Label("Billing"), billing);
-		Scene scene = new Scene(root);
+		Circle circle = new Circle(X, Y, RADIUS);
+		circle.setFill(CIRCLE_COLOR);
+		Pane pane = new Pane();
+		pane.getChildren().add(circle);
+		Scene scene = new Scene(pane);
+		circle.radiusProperty().bind(Bindings.divide(scene.widthProperty(), 2));
+		circle.centerXProperty().bind(Bindings.divide(scene.widthProperty(), 2));
+		circle.centerYProperty().bind(Bindings.divide(scene.heightProperty(), 2));
 		stage.setScene(scene);
 		stage.show();
 	}
