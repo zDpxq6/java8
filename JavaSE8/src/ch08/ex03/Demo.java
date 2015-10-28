eb223dfdf2357ec93b239b6be6ff9d423462761f
zDpxq6 <tsuguka@gmail.com>
2015-10-29 00:42:17 +0900

    added

---
+++ JavaSE8/src/ch08/ex03/Demo.java
@@ -0,0 +1,43 @@
+package ch08.ex03;
+
+import java.util.function.BinaryOperator;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// どれも同じ
+		System.out.println(calcurateGCD(-2, 4, (m, n) -> m % n));
+		System.out.println(calcurateGCD(-2, 4, Math::floorMod));
+		System.out.println(calcurateGCD(-2, 4, Integer::remainderUnsigned));
+	}
+
+	/**
+	 * 最大公約数を求める
+	 *
+	 * @param m
+	 *            整数, 0は許されない
+	 * @param n
+	 *            整数, 0は許されない
+	 * @param op
+	 *            余りを求める関数
+	 * @return 最大公約数
+	 */
+	public static final int calcurateGCD(int m, int n, BinaryOperator<Integer> op) {
+		if (op == null) {
+			throw new NullPointerException("a parameter \"op\" is null");
+		}
+		if (m == 0 || n == 0) {
+			throw new IllegalArgumentException("neither m nor n can be 0");
+		}
+		if (m < n) {// mとnを入れ替える
+			int tmp = n;
+			n = m;
+			m = tmp;
+		}
+		if (n == 0) {
+			return m;
+		} else {
+			return calcurateGCD(Math.abs(n), op.apply(m, n), op);
+		}
+	}
+}
---
+++ JavaSE8/src/ch08/ex04/Demo.java
@@ -0,0 +1,26 @@
+package ch08.ex04;
+
+import java.util.stream.LongStream;
+
+public class Demo {
+
+	private static final int A = 11;
+	private static final long V = 246154705703781l;
+	private static final long N = Double.valueOf(Math.pow(2, 48)).longValue();
+	private static final long M = 25214903917l;
+	private static final int MAX = 1000000;
+
+	public static void main(String[] args) {
+		long min = LongStream.iterate(0, Demo::prev).limit(MAX).reduce((l, m) -> l < m ? l : m).getAsLong();
+		System.out.println(min);
+	}
+
+	public static long next(long s) {
+		return s * M + A % N;
+	}
+
+	public static long prev(long s) {
+		return (s - A) * V % N;
+	}
+
+}
