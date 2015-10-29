ecff236cea8261e21132fd96ea6916f4b77945b3
zDpxq6 <tsuguka@gmail.com>
2015-10-29 23:57:32 +0900

    added

--- JavaSE8/src/ch02/demo/ch0203/Demo.java
+++ JavaSE8/src/ch02/demo/ch0203/Demo.java
@@ -1,51 +1,42 @@
 package ch02.demo.ch0203;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 //3. streamでなく, parallelStreamで長い単語を数えた場合の速度の違いを測りなさい.
 // 呼び出しの前後でSystem.nanotimeを呼び出して, 差を表示しなさい。
 // 高速なコンピュータを持っているのであれば, (『戦争と平和』(War and Peace)などの)もっと大きなドキュメントで試しなさい。
 public class Demo {
-	private static final int LENGTH_THRESHORLD = 11;
+	private static final int LENGTH_THRESHORLD = 12;
+	private static final int MAX = 1000;
 
 	public static void main(String[] args) throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("war and peace.txt")), StandardCharsets.UTF_8);
-		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-		
-		System.out.print("Parallel: ");
-		new ParallelMeasuring().measureTime(words, w -> LENGTH_THRESHORLD < w.length());
-		System.out.print("Single  : ");
-		new Measuring().measureTime(words, w -> LENGTH_THRESHORLD < w.length());
-	}
-}
+		String contents = new String(Files.readAllBytes(Paths.get("war and peace.txt")), StandardCharsets.UTF_8);
+		List<String> words = new ArrayList<String>(Arrays.asList(contents.split("[\\P{L}]+")));
+		long result = 0;
+		for (int i = 0; i < MAX; i++) {
+			result = measure(l -> l.stream().filter(w -> w.length() > LENGTH_THRESHORLD).count(), words);
+		}
+		System.out.println(result / MAX);
 
-interface Measure {
-	default void measureTime(List<String> words, Predicate<? super String> paramPredicate) {
-		long start = System.nanoTime();
-		measure(words, paramPredicate);
-		System.out.println(System.nanoTime() - start);
+		result = 0;
+		for (int i = 0; i < MAX; i++) {
+			result = measure(l -> l.removeIf(w -> w.length() <= LENGTH_THRESHORLD), words);
+		}
+		System.out.println(result / MAX);
+
 	}
 
-	void measure(List<String> words, Predicate<? super String> paramPredicate);
-}
-
-class Measuring implements Measure{
-	public void measure(List<String> words, Predicate<? super String> paramPredicate) {
-		words.stream().filter(paramPredicate).count();
-	}
-
-}
-
-class ParallelMeasuring implements Measure{
-	public void measure(List<String> words, Predicate<? super String> paramPredicate) {
-		words.parallelStream().filter(paramPredicate).count();
+	private static long measure(Function<List<String>, ?> consumer, List<String> l) {
+		long start = System.currentTimeMillis();
+		consumer.apply(l);
+		return System.currentTimeMillis() - start;
 	}
 }
---
+++ JavaSE8/src/ch08/ex05/Demo.java
@@ -0,0 +1,40 @@
+package ch08.ex05;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+//3. streamでなく, parallelStreamで長い単語を数えた場合の速度の違いを測りなさい.
+// 呼び出しの前後でSystem.nanotimeを呼び出して, 差を表示しなさい。
+// 高速なコンピュータを持っているのであれば, (『戦争と平和』(War and Peace)などの)もっと大きなドキュメントで試しなさい。
+public class Demo {
+	private static final int LENGTH_THRESHORLD = 12;
+	private static final int MAX = 1000;
+
+	public static void main(String[] args) throws IOException {
+		String contents = new String(Files.readAllBytes(Paths.get("war and peace.txt")), StandardCharsets.UTF_8);
+		List<String> words = new ArrayList<String>(Arrays.asList(contents.split("[\\P{L}]+")));
+		double result = 0;
+		for (int i = 0; i < MAX; i++) {
+			result += measure(l -> l.stream().filter(w -> w.length() > LENGTH_THRESHORLD).count(), words);
+		}
+		System.out.println(result / MAX);
+
+		result = 0;
+		for (int i = 0; i < MAX; i++) {
+			result += measure(l -> l.removeIf(w -> w.length() <= LENGTH_THRESHORLD), words);
+		}
+		System.out.println(result / MAX);
+
+	}
+
+	private static long measure(Function<List<String>, ?> consumer, List<String> l) {
+		long start = System.currentTimeMillis();
+		consumer.apply(l);
+		return System.currentTimeMillis() - start;
+	}
+}
