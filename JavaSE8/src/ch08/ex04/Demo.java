262e2375ff28cc9a6b9316cf8021444219b29357
zDpxq6 <tsuguka@gmail.com>
2015-11-20 10:59:21 +0900

    changed l to L

--- JavaSE8/src/ch08/ex04/Demo.java
+++ JavaSE8/src/ch08/ex04/Demo.java
@@ -5,7 +5,7 @@ import java.util.stream.LongStream;
 public class Demo {
 
 	private static final int A = 11;
-	private static final long V = 246154705703781l;
+	private static final long V = 246154705703781L;
 	private static final long N = Double.valueOf(Math.pow(2, 48)).longValue();
 	private static final long M = 25214903917l;
 	private static final int MAX = 1000000;
